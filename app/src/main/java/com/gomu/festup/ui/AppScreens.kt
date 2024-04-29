8b269b728d67943a236a1e9895079fa48cb967a5
Maits27 <maitiurlo@gmail.com>
2024-04-29 18:13:55 +0200

    Nav Screens

---
+++ app/src/main/java/com/gomu/festup/ui/AppScreens.kt
@@ -0,0 +1,24 @@
+package com.gomu.festup.ui
+
+sealed class AppScreens (val route: String) {
+    object LoginPage: AppScreens("LoginPage")
+    object App: AppScreens("App")
+
+    object PerfilYo: AppScreens("PerfilYo")
+    object PerfilUser: AppScreens("PerfilUser")
+    object PerfilCuadrilla: AppScreens("PerfilCuadrilla")
+
+    object Ajustes: AppScreens("Ajustes")
+    object EditPerfil: AppScreens("EditPerfil")
+
+    object Feed: AppScreens("Feed")
+    object Search: AppScreens("Search")
+    object EventsMap: AppScreens("EventsMap")
+    object EventsList: AppScreens("EventsList")
+
+    object Evento: AppScreens("Evento")
+
+    object AddCuadrilla: AppScreens("AddCuadrilla")
+    object AddEvento: AppScreens("AddEvento")
+
+}
