0a15fdf936fde479c5f80691e43a256c4ac1998b
Maits27 <maitiurlo@gmail.com>
2024-05-16 18:04:44 +0200

    fcm

--- app/src/main/java/com/gomu/festup/utils/FCMService.kt
+++ app/src/main/java/com/gomu/festup/utils/FCMService.kt
@@ -13,6 +13,11 @@ import com.gomu.festup.data.repositories.preferences.IGeneralPreferences
 import com.gomu.festup.data.repositories.preferences.ILoginSettings
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
@@ -26,6 +31,7 @@ class FCMService : FirebaseMessagingService() {
 
     @Inject
     lateinit var preferences: IGeneralPreferences
+    @Inject
     lateinit var loginSettings: ILoginSettings
 
     /**
@@ -34,25 +40,38 @@ class FCMService : FirebaseMessagingService() {
      * @param remoteMessage El mensaje de notificación.
      */
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val context = this
+        CoroutineScope(Dispatchers.IO).launch {
+            val currentUser = loginSettings.getLastLoggedUser()
+            if(currentUser!="") {
+                if (preferences.getReceiveNotifications(currentUser).first()) {
+                    withContext(Dispatchers.Default) {
+                        remoteMessage.notification?.let { notification ->
 
-        remoteMessage.notification?.let { notification ->
+                            Log.d("FCM", "Message Notification Title: ${notification.title}")
+                            Log.d("FCM", "Message Notification Body: ${notification.body}")
 
-            Log.d("FCM", "Message Notification Title: ${notification.title}")
-            Log.d("FCM", "Message Notification Body: ${notification.body}")
-
-            val builder = NotificationCompat.Builder(this, MainActivity.CURRENT_CHANNEL)
-                .setSmallIcon(R.drawable.festup)
-                .setContentTitle(notification.title)
-                .setContentText(notification.body)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true)
-            try {
-                with(NotificationManagerCompat.from(this)) {
-                    notify(NotificationID.NOTIFICATIONS.id, builder.build())
+                            val builder = NotificationCompat.Builder(
+                                context,
+                                MyNotificationChannels.NOTIFICATIONS_CHANNEL.name
+                            )
+                                .setSmallIcon(R.drawable.festup)
+                                .setContentTitle(notification.title)
+                                .setContentText(notification.body)
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setAutoCancel(true)
+                            try {
+                                with(NotificationManagerCompat.from(context)) {
+                                    notify(NotificationID.NOTIFICATIONS.id, builder.build())
+                                }
+                            } catch (e: SecurityException) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
                 }
-            } catch (e: SecurityException) {
-                e.printStackTrace()
             }
         }
+
     }
 }
